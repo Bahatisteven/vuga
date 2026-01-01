299adbe60be61f32b279b746c8beb6376e83ac2d
Bahatisteven <152080755+Bahatisteven@users.noreply.github.com>
2026-01-01 21:30:08 +0200

    chore: enabled validation globally

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 /**
  * entry point
@@ -8,6 +9,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/v1/api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   const port = process.env.PORT!;
   await app.listen(port);

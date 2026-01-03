2153bb9b006d18e27459a1bbacdfb9c152e97c06
Bahatisteven <152080755+Bahatisteven@users.noreply.github.com>
2026-01-03 18:21:22 +0200

    feat: added auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Request,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { CreateUserDto } from '../user/dto';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
+
+  /**register new user  */
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  async register(@Body() createUserDto: CreateUserDto) {
+    const user = await this.userService.create(createUserDto);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { passwordHash, ...result } = user;
+    return {
+      message: 'User registered successfully',
+      user: result,
+    };
+  }
+
+  /**login */
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+}

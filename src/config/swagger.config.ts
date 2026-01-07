eae89a840746be68cde8dd11c52d99873897acfe
Bahatisteven <152080755+Bahatisteven@users.noreply.github.com>
2026-01-07 23:54:18 +0200

    chore: removed description for better visuals

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -2,7 +2,6 @@ import { DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('Vuga Platform API')
-  .setDescription('Real-time Voice Translator API')
   .setVersion('1.0.0')
   .addBearerAuth()
   .addTag('auth', 'Authentication endpoints')

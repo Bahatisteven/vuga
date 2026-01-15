434279a6ce01dafdfca24daa218841f28f5cb01e
Bahatisteven <152080755+Bahatisteven@users.noreply.github.com>
2026-01-15 20:31:40 +0200

    chore(modules): add Redis provider

--- src/translation/translation.module.ts
+++ src/translation/translation.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TranslationService } from './translation.service';
 import { TranslationController } from './translation.controller';
+import { RedisProvider } from './redis.provider';
 
 @Module({
-  providers: [TranslationService],
+  providers: [TranslationService, RedisProvider],
   controllers: [TranslationController],
+  exports: [TranslationService],
 })
 export class TranslationModule {}
